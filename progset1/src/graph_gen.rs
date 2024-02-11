d55e18db745ae10b66d646d286db9d0499087d45
cfzimmerman <coryzimmerman93@gmail.com>
2024-02-10 22:52:41 -0500

    progset 1: generate graphs of dimensions 1 to 4

--- progset1/src/graph_gen.rs
+++ progset1/src/graph_gen.rs
@@ -1,4 +1,4 @@
-use crate::{decimal::Decimal, error::PsetRes, prim_heap::Weight};
+use crate::{decimal::Decimal, prim_heap::Weight};
 use rand::{rngs::ThreadRng, thread_rng, Rng};
 use std::{
     collections::{HashMap, HashSet},
@@ -36,6 +36,21 @@ pub struct Vertex2D {
     y: Decimal,
 }
 
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct Vertex3D {
+    x: Decimal,
+    y: Decimal,
+    z: Decimal,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct Vertex4D {
+    r: Decimal,
+    g: Decimal,
+    b: Decimal,
+    a: Decimal,
+}
+
 pub type Edges<V> = HashSet<WeightedEdge<V>>;
 pub type Graph<V> = HashMap<Rc<V>, Edges<Rc<V>>>;
 
@@ -58,15 +73,20 @@ impl CompleteUnitGraph {
             }));
         }
 
-        // Add edges so that it's a complete graph
+        // Add edges so that it's a complete graph.
+        // Initialize the weights to -1. They'll be updated once
+        // all edges are in place.
         for v_from in vertices.iter() {
             for v_to in vertices.iter() {
                 if v_from == v_to {
                     continue;
                 }
+                // Ensure both sides of an edge have the same weight
+                let weight = Self::get_existing_weight(&mut graph, &v_from, &v_to)
+                    .unwrap_or_else(|| rng.gen::<f64>().into());
                 let new_edge = WeightedEdge {
                     vertex: v_to.clone(),
-                    weight: rng.gen::<f64>().into(),
+                    weight,
                 };
                 if let Some(edges) = graph.get_mut(v_from) {
                     edges.insert(new_edge);
@@ -75,13 +95,14 @@ impl CompleteUnitGraph {
                 graph.insert(v_from.clone(), [new_edge].into());
             }
         }
+
         graph
     }
 
     /// Generates a complete n dimensional graph where each coordinate is generated
     /// by calling T::new_rand and edge distances are the distance between
     /// points.
-    pub fn graph_nd<V>(num_vertices: usize) -> PsetRes<Graph<V>>
+    pub fn graph_nd<V>(num_vertices: usize) -> Graph<V>
     where
         V: VertexCoord<V> + PartialEq + Eq + Hash,
     {
@@ -92,7 +113,7 @@ impl CompleteUnitGraph {
         // need to retry if duplicates are created. However, limit that to at
         // most 4x tries total to prevent an infinite loop somehow.
         for _ in 0..(num_vertices * 4) {
-            if vertices.len() > num_vertices {
+            if num_vertices <= vertices.len() {
                 break;
             }
             vertices.insert(Rc::new(V::new_rand(&mut rng)));
@@ -104,9 +125,11 @@ impl CompleteUnitGraph {
                 if v_from == v_to {
                     continue;
                 }
+                let weight = Self::get_existing_weight(&mut graph, &v_from, &v_to)
+                    .unwrap_or_else(|| v_from.dist(v_to).into());
                 let new_edge = WeightedEdge {
                     vertex: v_to.clone(),
-                    weight: v_from.dist(v_to).into(),
+                    weight,
                 };
                 if let Some(edges) = graph.get_mut(v_from) {
                     edges.insert(new_edge);
@@ -115,7 +138,36 @@ impl CompleteUnitGraph {
                 graph.insert(v_from.clone(), [new_edge].into());
             }
         }
-        Ok(graph)
+        graph
+    }
+
+    /// Before adding a new edge, this function checks if a weighted edge
+    /// beginning on the other side already exists. If so, that weight is
+    /// returned. If not, None is returned.
+    /// Even for n-dimensional graphs, this is helpful to avoid float
+    /// imprecision when recalculating distance.
+    ///
+    /// The old_to, old_from naming implies that the caller is now suggesting
+    /// an edge from old_to to old_from.
+    fn get_existing_weight<V>(
+        graph: &mut Graph<V>,
+        old_to: &Rc<V>,
+        old_from: &Rc<V>,
+    ) -> Option<Weight<f64>>
+    where
+        V: PartialEq + Eq + Hash,
+    {
+        if let Some(other_side) = graph.get(old_from) {
+            let target_edge = WeightedEdge {
+                vertex: old_to.clone(),
+                // Equality on edges doesn't check weights
+                weight: Weight::new(-1.),
+            };
+            if let Some(existing_edge) = other_side.get(&target_edge) {
+                return Some(existing_edge.weight);
+            }
+        }
+        None
     }
 }
 
@@ -141,7 +193,20 @@ where
 
 impl VertexCoord<Vertex2D> for Vertex2D {
     fn dist(&self, other: &Vertex2D) -> f64 {
-        ((&other.x - &self.x).get().powi(2) + (&other.y - &self.y).get().powi(2)).sqrt()
+        let dst = [&other.x - &self.x, &other.y - &self.y]
+            .into_iter()
+            .map(|num| num.get().powi(2))
+            .sum::<f64>()
+            .sqrt();
+        // Verifies correctness at 2d. Correctness beyond 2d is assumed because of this.
+        debug_assert_eq!(
+            Decimal::new(dst),
+            Decimal::new(
+                ((&other.x - &self.x).get().powi(2) + (&other.y - &self.y).get().powi(2)).sqrt()
+            ),
+            "Iterable 2d dist should equal formula 2d dist."
+        );
+        dst
     }
 
     fn new_rand(rng: &mut ThreadRng) -> Vertex2D {
@@ -152,9 +217,53 @@ impl VertexCoord<Vertex2D> for Vertex2D {
     }
 }
 
+impl VertexCoord<Vertex3D> for Vertex3D {
+    fn dist(&self, other: &Vertex3D) -> f64 {
+        [&other.x - &self.x, &other.y - &self.y, &other.z - &self.z]
+            .into_iter()
+            .map(|num| num.get().powi(2))
+            .sum::<f64>()
+            .sqrt()
+    }
+
+    fn new_rand(rng: &mut ThreadRng) -> Vertex3D {
+        Vertex3D {
+            x: rng.gen::<f64>().into(),
+            y: rng.gen::<f64>().into(),
+            z: rng.gen::<f64>().into(),
+        }
+    }
+}
+
+impl VertexCoord<Vertex4D> for Vertex4D {
+    fn dist(&self, other: &Vertex4D) -> f64 {
+        [
+            &other.r - &self.r,
+            &other.g - &self.g,
+            &other.b - &self.b,
+            &other.a - &self.a,
+        ]
+        .into_iter()
+        .map(|num| num.get().powi(2))
+        .sum::<f64>()
+        .sqrt()
+    }
+
+    fn new_rand(rng: &mut ThreadRng) -> Vertex4D {
+        Vertex4D {
+            r: rng.gen::<f64>().into(),
+            g: rng.gen::<f64>().into(),
+            b: rng.gen::<f64>().into(),
+            a: rng.gen::<f64>().into(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod graph_tests {
-    use super::{CompleteUnitGraph, Graph};
+    use super::{CompleteUnitGraph, Graph, Vertex2D, Vertex3D, Vertex4D, WeightedEdge};
+    use crate::prim_heap::Weight;
+    use std::hash::Hash;
 
     /// Returns the number of undirected edges in the graph and panics
     /// if there's an odd number of edges.
@@ -168,17 +277,68 @@ mod graph_tests {
         total / 2
     }
 
+    /// Verifies that the graph has the edge counts of a complete, undirected
+    /// graph.
+    fn assert_complete_graph<V>(graph: &Graph<V>, size: usize) {
+        assert_eq!(graph.len(), size, "Graph size should equal input");
+        let expected_num_edges = (size * (size - 1)) / 2;
+        assert_eq!(
+            expected_num_edges,
+            count_edges(&graph),
+            "Graph edges should indicate completeness"
+        );
+    }
+
+    /// Verifies every edge in the graph has a counterpart in the
+    /// other direction and that both sides have the same weight.
+    fn assert_edges_well_defined<V>(graph: &Graph<V>)
+    where
+        V: PartialEq + Eq + Hash,
+    {
+        for (vertex, edges) in graph.iter() {
+            for edge in edges.iter() {
+                let other_side_weight = &graph
+                    .get(&edge.vertex)
+                    .expect("Referenced other vertex should be in the graph")
+                    .get(&WeightedEdge {
+                        vertex: vertex.clone(),
+                        // Again, equality only looks at the vertex.
+                        weight: Weight::new(-1.),
+                    })
+                    .expect("The other vertex should have an edge pointing to curr vertex")
+                    .weight;
+                // While comparing floats is flakey, graph generation should ensure they're
+                // identical.
+                assert_eq!(&edge.weight, other_side_weight);
+            }
+        }
+    }
+
+    /// Generates all graph sizes and verifies invariants about them
     #[test]
-    fn gen_graph_1d() {
-        for size in 2..16 {
-            let graph = CompleteUnitGraph::graph_1d(size);
-            assert_eq!(graph.len(), size, "Graph size should equal input");
-            let expected_num_edges = (size * (size - 1)) / 2;
-            assert_eq!(
-                expected_num_edges,
-                count_edges(&graph),
-                "Graph edges should indicate completeness"
-            );
+    fn gen_graphs() {
+        for size in 2..=16 {
+            // The subscopes prevent copy-paste accidentally testing the wrong graph
+            {
+                let graph_1d = CompleteUnitGraph::graph_1d(size);
+                assert_complete_graph(&graph_1d, size);
+                assert_edges_well_defined(&graph_1d);
+            }
+            {
+                let graph_2d = CompleteUnitGraph::graph_nd::<Vertex2D>(size);
+                assert_complete_graph(&graph_2d, size);
+                assert_edges_well_defined(&graph_2d);
+            }
+            {
+                let graph_3d = CompleteUnitGraph::graph_nd::<Vertex3D>(size);
+                assert_complete_graph(&graph_3d, size);
+                assert_edges_well_defined(&graph_3d);
+            }
+            {
+                let graph_4d = CompleteUnitGraph::graph_nd::<Vertex4D>(size);
+                assert_complete_graph(&graph_4d, size);
+                assert_edges_well_defined(&graph_4d);
+            }
         }
     }
 }
