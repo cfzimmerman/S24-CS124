6d606cf8d07ebcd87c06a8882744370111a820fe
cfzimmerman <coryzimmerman93@gmail.com>
2024-03-05 00:03:33 -0500

    ps4: init q3b

--- ps4/src/lib.rs
+++ ps4/src/lib.rs
@@ -1,10 +1,11 @@
-use std::ops::RangeInclusive;
+use std::{fmt::Display, ops::RangeInclusive};
 
 #[derive(Debug)]
 pub struct PrettyPrint<'a, T> {
     pref_len: usize,
     text: &'a [T],
     line_breaks: Vec<usize>,
+    total_cost: i32,
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -15,13 +16,14 @@ struct Cached {
 
 impl<'a, T> PrettyPrint<'a, T>
 where
-    T: AsRef<str>,
+    T: AsRef<str> + Display,
 {
     pub fn new(preferred_len: usize, text: &'a [T]) -> Self {
         PrettyPrint {
             pref_len: preferred_len,
             text,
             line_breaks: Vec::new(),
+            total_cost: 0,
         }
     }
 
@@ -30,14 +32,21 @@ where
     pub fn find_pretty(&mut self) -> anyhow::Result<()> {
         let mut dp = vec![Cached::default(); self.text.len()];
 
-        for (startline, word) in self.text.iter().rev().enumerate() {
+        if let (Some(last_word), Some(last_dp)) = (self.text.last(), dp.last_mut()) {
+            // base case
+            let last_ind = self.text.len() - 1;
+            last_dp.cost = self.cost(last_ind..=last_ind, last_word.as_ref().len())?;
+        }
+        for startline in (0..self.text.len()).rev() {
             let mut chars_in_range: usize = 0;
-            for eol in (startline + 1)..self.text.len() {
-                chars_in_range += word.as_ref().len();
+            for (eol, end_word) in self.text.iter().enumerate().skip(startline) {
+                chars_in_range += end_word.as_ref().len();
                 let cost_of_interval = self.cost(startline..=eol, chars_in_range)?;
+                if cost_of_interval >= dp[startline].cost {
+                    continue;
+                }
                 let cost_of_rest = dp.get(eol + 1).map(|c| c.cost).unwrap_or(0);
                 let cost_if_break_here = cost_of_interval + cost_of_rest;
-
                 if cost_if_break_here < dp[startline].cost {
                     dp[startline] = Cached {
                         cost: cost_if_break_here,
@@ -50,14 +59,50 @@ where
         Ok(())
     }
 
+    /// Pretty-prints the input. If find_pretty has not been called, there will be
+    /// no added line breaks.
+    pub fn print(&self) -> anyhow::Result<()> {
+        let mut formatted = String::with_capacity(self.line_breaks.len() * self.pref_len);
+        let mut line_break_iter = self.line_breaks.iter();
+        let mut next_line_break = line_break_iter.next();
+
+        for (ind, word) in self.text.iter().enumerate() {
+            if let Some(br) = next_line_break {
+                if &ind == br {
+                    formatted.push('\n');
+                    next_line_break = line_break_iter.next();
+                }
+            }
+            formatted.push_str(word.as_ref());
+            formatted.push(' ');
+        }
+        println!("{formatted}");
+        println!("total cost: {}", self.total_cost);
+        Ok(())
+    }
+
     /// Given the DP array for the current paragraph, searches the array and extracts
     /// line breaks for printing from the first word.
     fn add_line_breaks(&mut self, dp: &[Cached]) {
-        let mut next_line = dp.get(0).map(|entry| entry.next_line).flatten();
+        let mut next_line = dp
+            .get(0)
+            .map(|entry| {
+                self.total_cost += entry.cost;
+                entry.next_line
+            })
+            .flatten();
         while let Some(ind) = next_line {
+            self.total_cost += dp[ind].cost;
             self.line_breaks.push(ind);
             next_line = dp.get(ind).map(|entry| entry.next_line).flatten();
         }
+    }
+
+    /// Prints a benchmark for how wide the preferred window of text is
+    pub fn print_preference(&self) -> anyhow::Result<()> {
+        let chars: Vec<u8> = vec!['#'.try_into()?; self.pref_len];
+        println!("\n{}", String::from_utf8(chars)?);
+        Ok(())
     }
 
     /// Returns the cost of a line bounded by the given range of words with words
@@ -69,12 +114,17 @@ where
             char_ct.try_into()?,
             self.pref_len.try_into()?,
         );
-        // end is incremented because the formula assumes inclusive
-        let closeness: i32 = pref_len - (end + 1) + start - char_ct;
-        if closeness < 0 {
-            return Ok(2i32.pow(closeness.abs().try_into()?) - closeness.pow(3) - 1);
+        let closeness: i32 = pref_len - end + start - char_ct;
+        if closeness >= 0 && words.end() + 1 == self.text.len() {
+            // "The last line has no penalties if A >= 0"
+            return Ok(0);
         }
-        Ok(closeness.pow(3).into())
+        if closeness < 0 {
+            let first_term = 2i32.saturating_pow(closeness.abs().try_into()?);
+            let second_term = closeness.saturating_pow(3);
+            return Ok(first_term.saturating_sub(second_term).saturating_sub(1));
+        }
+        Ok(closeness.saturating_pow(3).into())
     }
 }
 
@@ -84,5 +134,67 @@ impl Default for Cached {
             cost: i32::MAX,
             next_line: None,
         }
+    }
+}
+
+#[cfg(test)]
+mod test_pretty {
+    use crate::PrettyPrint;
+    use std::fs;
+
+    /// Retrieves strings from text files
+    fn get_test_texts() -> anyhow::Result<Vec<String>> {
+        let files = ["./test_data/bee.txt", "./test_data/yes.txt"];
+        let mut res = Vec::with_capacity(files.len());
+        for path in files {
+            res.push(fs::read_to_string(path)?);
+        }
+        Ok(res)
+    }
+
+    /// Verifies that the number of line breaks decreases as the
+    /// line length preference increases.
+    #[test]
+    fn breaks_correspond_to_pref() -> anyhow::Result<()> {
+        for text in get_test_texts()? {
+            let mut prev_break_ct = usize::MAX;
+            let text: Vec<&str> = text.split(' ').collect();
+            for preference in [17, 32, 64] {
+                let mut pretty = PrettyPrint::new(preference, &text);
+                pretty.find_pretty()?;
+                assert!(
+                    pretty.line_breaks.len() < prev_break_ct,
+                    "line break counts should decrease as line preference increases"
+                );
+                prev_break_ct = pretty.line_breaks.len();
+                pretty.print_preference()?;
+                pretty.print()?;
+            }
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn cost() -> anyhow::Result<()> {
+        let text = ["aa", "bb", "cc", "dd", "ee"];
+        {
+            let mut pretty = PrettyPrint::new(2, &text);
+            pretty.find_pretty()?;
+            assert!(
+                pretty.total_cost == 0,
+                "preference 2: input should each have its own line"
+            );
+        }
+        {
+            let mut pretty = PrettyPrint::new(5, &text);
+            pretty.find_pretty()?;
+            // pretty.print_preference()?;
+            // pretty.print()?;
+            assert!(
+                pretty.total_cost == 0,
+                "preference 5: input should still print perfectly"
+            );
+        }
+        Ok(())
     }
 }
--- ps4/src/main.rs
+++ ps4/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use ps4::PrettyPrint;
+use std::{
+    env::args,
+    io::{self},
+};
+
+fn usage() {
+    println!("usage: cargo run [int preference]");
+    println!("ex: cat test_data/yes.txt | cargo run 17");
+}
+
+/// Takes a CLI argument for the preferred character width of a line.
+/// Reads text from stdin and pretty prints it given the character preference.
+fn main() -> anyhow::Result<()> {
+    let Some(pref_len) = args()
+        .skip(1)
+        .next()
+        .map(|arg| arg.parse::<usize>().ok())
+        .flatten()
+    else {
+        usage();
+        return Ok(());
+    };
+
+    let input: Vec<String> = io::stdin().lines().filter_map(Result::ok).collect();
+    let trimmed: Vec<&str> = input
+        .iter()
+        .flat_map(|txt| txt.split_whitespace())
+        .collect();
+
+    let mut pretty = PrettyPrint::new(pref_len, &trimmed);
+    pretty.find_pretty()?;
+    pretty.print_preference()?;
+    pretty.print()?;
+
+    Ok(())
 }
