5eaa6e37305311800e2d304052aba0e060c4aa72
cfzimmerman <coryzimmerman93@gmail.com>
2024-03-23 00:21:07 -0400

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-git checkout $1 
-  && cd $2 && cd .. # verify that arg 2 is a valid directory
-  && rm -rf (ls | grep -v $2) 
-  && cd $2 
-  && mv . ..
-  && cd ..
-  && rmdir $2
-
+git checkout $1 &&
+	cd $2 && cd .. # verify that arg 2 is a valid directory
+#  && rm -rf (ls | grep -v $2)
+#  && cd $2
+#  && mv . ..
+#  && cd ..
+#  && rmdir $2
