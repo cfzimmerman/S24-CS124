96ce8d3f3efdad27b45418340e3d0502228e65a4
cfzimmerman <coryzimmerman93@gmail.com>
2024-03-23 00:26:05 -0400

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,11 @@ fi
 
 git checkout $1 &&
 	git fetch origin &&
-	cd $2 && # verify that arg 2 is a valid directory 
-  cd .. && 
-  git reset --hard origin/main &&
-  rm -rf (ls | grep -v $2) && 
-  cd $2 && 
-  mv . .. && 
-  cd .. && 
-  rmdir $2
+	cd $2 && # verify that arg 2 is a valid directory
+	cd .. &&
+	git reset --hard origin/main &&
+	rm -rf $(ls | grep -v $2) &&
+	cd $2 &&
+	mv . .. &&
+	cd .. &&
+	rmdir $2
