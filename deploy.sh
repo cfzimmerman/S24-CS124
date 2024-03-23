2596aa3a8a9c5841f847e4073555acc8da62266b
cfzimmerman <coryzimmerman93@gmail.com>
2024-03-23 00:25:41 -0400

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+if [ "$1" = "main" ]; then
+	echo "Cannot deploy from main"
+	exit 1
+fi
+
 git checkout $1 &&
-	cd $2 && cd .. # verify that arg 2 is a valid directory
-#  && rm -rf (ls | grep -v $2)
-#  && cd $2
-#  && mv . ..
-#  && cd ..
-#  && rmdir $2
+	git fetch origin &&
+	cd $2 && # verify that arg 2 is a valid directory 
+  cd .. && 
+  git reset --hard origin/main &&
+  rm -rf (ls | grep -v $2) && 
+  cd $2 && 
+  mv . .. && 
+  cd .. && 
+  rmdir $2
